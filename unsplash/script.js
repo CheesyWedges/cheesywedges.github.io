0c761328e00ab567576264c83631afc2d419893c
KitKetu <kitketu@users.noreply.github.com>
2022-08-17 14:08:00 +0500

    Added photographer's name below the photos

--- unsplash/script.js
+++ unsplash/script.js
@@ -1,16 +1,20 @@
 
 const url = "https://api.unsplash.com/photos?client_id=LGEveGm-PeBjwONWY20yOZti3DFE3J1i01UVn6lpfKg"
-const url2 = "https://api.unsplash.com/search/photos?query=sensual&client_id=LGEveGm-PeBjwONWY20yOZti3DFE3J1i01UVn6lpfKg"
 
+const url2 = "https://api.unsplash.com/search/photos?query=sensual&client_id=LGEveGm-PeBjwONWY20yOZti3DFE3J1i01UVn6lpfKg"
+    
 fetch(url)
     .then(response => response.json())
     .then(data => {
 
-        /* Display 10 photos returned by default */
+        /* Display 10 photos returned */
         for(let i=0; i<data.length; i++) {
-            document.getElementById("contents").innerHTML += `
-            <img src="${data[i].urls.regular}" alt="" >
-        `
+            document.getElementById("contents3").innerHTML += `
+                <div>
+                    <img src="${data[i].urls.regular}" alt="" >
+                    <p>Photo by: ${data[i].user.name}</p>
+                </div>
+            `
         }
     })
 
@@ -23,8 +27,11 @@ fetch(url)
 
         for (let i = 0; i < postsArray.length; i++) {
             document.getElementById("contents2").innerHTML += `
-        <img src="${postsArray[i].urls.regular}" alt="" >
-    `
+                <div>
+                    <img src="${postsArray[i].urls.regular}" alt="" > 
+                    <p>Photo by: ${postsArray[i].user.name}</p> 
+                </div>    
+            `
         }
     })
 
@@ -35,8 +42,9 @@ fetch(url2)
         /*  Get random number between 0 and 9 */
         let rand = Math.floor(Math.random()*10)
 
-        /* Display random photo from the results */
-        document.getElementById("contents3").innerHTML += `
+        /* Display 1 photo based on search query */
+        document.getElementById("contents").innerHTML += `
             <img src="${data.results[rand].urls.regular}" alt="" >
+            <p>Photo by: ${data.results[rand].user.name}</p>
         `
     })
